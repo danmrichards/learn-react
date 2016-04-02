d4c76d2d9f257979977d53808994218e9a7b30bc
Dan Richards <dan.m.richards@gmail.com>
2016-04-02 22:19:28 +0100

    Lesson 3 - From Internal to External Scripts

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Learn React
 
 React training based on https://laracasts.com/series/do-you-react
+
+## Notes
+
+Lesson 3 requires a server; quickest way to do this is to just run a local PHP server:
+
+```
+php -S localhost:8885
+```
+
+And then go to http://localhost:8885/03-inline-to-external-scripts.html in the browser.
---
+++ js/components/Avatar.js
@@ -0,0 +1,18 @@
+var Avatar = React.createClass({
+    getDefaultProps: function() {
+        return {
+            path: 'http://placehold.it/98x98',
+        };
+    },
+    render: function() {
+        return (
+            <div>
+                <a href={this.props.path}>
+                    <img src={this.props.path} alt="Avatar"/>
+                </a>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<Avatar path="http://lorempixel.com/98/98/" />, document.body);
